0a6d46287166ae2f4e486c52731928c47001fb57
Даниил Лямин <Niger120404@yandex.ru>
2022-06-12 02:28:36 +0300

    +

--- BIVT-21-15_Liamin-Borodin.D.A_DZ_1/Form1.Designer.cs
+++ BIVT-21-15_Liamin-Borodin.D.A_DZ_1/Form1.Designer.cs
@@ -40,20 +40,6 @@
             this.button13 = new System.Windows.Forms.Button();
             this.button14 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.textBox9 = new System.Windows.Forms.TextBox();
-            this.textBox10 = new System.Windows.Forms.TextBox();
-            this.textBox11 = new System.Windows.Forms.TextBox();
-            this.textBox12 = new System.Windows.Forms.TextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -190,7 +176,7 @@
             // 
             this.button14.Location = new System.Drawing.Point(330, 151);
             this.button14.Name = "button14";
-            this.button14.Size = new System.Drawing.Size(100, 524);
+            this.button14.Size = new System.Drawing.Size(150, 524);
             this.button14.TabIndex = 13;
             this.button14.Text = "=";
             this.button14.UseVisualStyleBackColor = true;
@@ -201,157 +187,16 @@
             this.richTextBox1.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.richTextBox1.Location = new System.Drawing.Point(12, 12);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(418, 133);
+            this.richTextBox1.Size = new System.Drawing.Size(468, 133);
             this.richTextBox1.TabIndex = 14;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(436, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(65, 34);
-            this.textBox1.TabIndex = 15;
-            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox2.Location = new System.Drawing.Point(507, 12);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(65, 34);
-            this.textBox2.TabIndex = 16;
-            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
-            // 
-            // textBox3
-            // 
-            this.textBox3.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox3.Location = new System.Drawing.Point(578, 12);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(65, 34);
-            this.textBox3.TabIndex = 17;
-            this.textBox3.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
-            // 
-            // textBox4
-            // 
-            this.textBox4.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox4.Location = new System.Drawing.Point(649, 12);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(65, 34);
-            this.textBox4.TabIndex = 18;
-            this.textBox4.TextChanged += new System.EventHandler(this.TextBox4_TextChanged);
-            // 
-            // textBox5
-            // 
-            this.textBox5.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox5.Location = new System.Drawing.Point(436, 52);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(65, 34);
-            this.textBox5.TabIndex = 19;
-            this.textBox5.TextChanged += new System.EventHandler(this.TextBox5_TextChanged);
-            // 
-            // textBox6
-            // 
-            this.textBox6.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox6.Location = new System.Drawing.Point(507, 52);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(65, 34);
-            this.textBox6.TabIndex = 20;
-            this.textBox6.TextChanged += new System.EventHandler(this.TextBox6_TextChanged);
-            // 
-            // textBox7
-            // 
-            this.textBox7.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox7.Location = new System.Drawing.Point(578, 52);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(65, 34);
-            this.textBox7.TabIndex = 21;
-            this.textBox7.TextChanged += new System.EventHandler(this.TextBox7_TextChanged);
-            // 
-            // textBox8
-            // 
-            this.textBox8.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox8.Location = new System.Drawing.Point(649, 52);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(65, 34);
-            this.textBox8.TabIndex = 22;
-            this.textBox8.TextChanged += new System.EventHandler(this.TextBox8_TextChanged);
-            // 
-            // textBox9
-            // 
-            this.textBox9.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox9.Location = new System.Drawing.Point(436, 92);
-            this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(65, 34);
-            this.textBox9.TabIndex = 23;
-            this.textBox9.TextChanged += new System.EventHandler(this.TextBox9_TextChanged);
-            // 
-            // textBox10
-            // 
-            this.textBox10.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox10.Location = new System.Drawing.Point(507, 92);
-            this.textBox10.Name = "textBox10";
-            this.textBox10.Size = new System.Drawing.Size(65, 34);
-            this.textBox10.TabIndex = 24;
-            this.textBox10.TextChanged += new System.EventHandler(this.TextBox10_TextChanged);
-            // 
-            // textBox11
-            // 
-            this.textBox11.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox11.Location = new System.Drawing.Point(578, 92);
-            this.textBox11.Name = "textBox11";
-            this.textBox11.Size = new System.Drawing.Size(65, 34);
-            this.textBox11.TabIndex = 25;
-            this.textBox11.TextChanged += new System.EventHandler(this.TextBox11_TextChanged);
-            // 
-            // textBox12
-            // 
-            this.textBox12.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox12.Location = new System.Drawing.Point(649, 92);
-            this.textBox12.Name = "textBox12";
-            this.textBox12.Size = new System.Drawing.Size(65, 34);
-            this.textBox12.TabIndex = 26;
-            this.textBox12.TextChanged += new System.EventHandler(this.TextBox12_TextChanged);
-            // 
-            // richTextBox2
-            // 
-            this.richTextBox2.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.richTextBox2.Location = new System.Drawing.Point(436, 151);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(278, 524);
-            this.richTextBox2.TabIndex = 27;
-            this.richTextBox2.Text = "";
-            this.richTextBox2.TextChanged += new System.EventHandler(this.RichTextBox2_TextChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(554, 129);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(48, 20);
-            this.label1.TabIndex = 28;
-            this.label1.Text = "Ответ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(732, 687);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.richTextBox2);
-            this.Controls.Add(this.textBox12);
-            this.Controls.Add(this.textBox11);
-            this.Controls.Add(this.textBox10);
-            this.Controls.Add(this.textBox9);
-            this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(492, 687);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button14);
             this.Controls.Add(this.button13);
@@ -371,10 +216,8 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Calculator";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
-
         #endregion
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
@@ -391,19 +234,5 @@
         private System.Windows.Forms.Button button13;
         private System.Windows.Forms.Button button14;
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox8;
-        private System.Windows.Forms.TextBox textBox9;
-        private System.Windows.Forms.TextBox textBox10;
-        private System.Windows.Forms.TextBox textBox11;
-        private System.Windows.Forms.TextBox textBox12;
-        private System.Windows.Forms.RichTextBox richTextBox2;
-        private System.Windows.Forms.Label label1;
     }
 }
--- BIVT-21-15_Liamin-Borodin.D.A_DZ_1/Form1.cs
+++ BIVT-21-15_Liamin-Borodin.D.A_DZ_1/Form1.cs
@@ -11,17 +11,17 @@ namespace BIVT_21_15_Liamin_Borodin.D.A_DZ_1
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            richTextBox1.Text += " ";
+            richTextBox1.AppendText(text: " ");
         }
 
         private void Button2_Click(object sender, EventArgs e)
         {
-            richTextBox1.Text = "";
+            richTextBox1.Clear();
         }
 
         private void Button3_Click(object sender, EventArgs e)
         {
-            richTextBox1.Text += "0";
+            richTextBox1.AppendText(text: "0");
         }
 
         private void Button4_Click(object sender, EventArgs e)
@@ -32,7 +32,7 @@ namespace BIVT_21_15_Liamin_Borodin.D.A_DZ_1
             }
             else
             {
-                richTextBox1.Text += "-";
+                richTextBox1.AppendText(text: "-");
             }
         }
 
@@ -44,7 +44,7 @@ namespace BIVT_21_15_Liamin_Borodin.D.A_DZ_1
             }
             else
             {
-                richTextBox1.AppendText("1");
+                richTextBox1.AppendText(text: "1");
             }
         }
 
@@ -56,7 +56,7 @@ namespace BIVT_21_15_Liamin_Borodin.D.A_DZ_1
             }
             else
             {
-                richTextBox1.AppendText("2");
+                richTextBox1.AppendText(text: "2");
             }
         }
 
@@ -68,7 +68,7 @@ namespace BIVT_21_15_Liamin_Borodin.D.A_DZ_1
             }
             else
             {
-                richTextBox1.AppendText("3");
+                richTextBox1.AppendText(text: "3");
             }
         }
 
@@ -80,7 +80,7 @@ namespace BIVT_21_15_Liamin_Borodin.D.A_DZ_1
             }
             else
             {
-                richTextBox1.AppendText("4");
+                richTextBox1.AppendText(text: "4");
             }
         }
 
@@ -92,7 +92,7 @@ namespace BIVT_21_15_Liamin_Borodin.D.A_DZ_1
             }
             else
             {
-                richTextBox1.AppendText("5");
+                richTextBox1.AppendText(text: "5");
             }
         }
 
@@ -104,7 +104,7 @@ namespace BIVT_21_15_Liamin_Borodin.D.A_DZ_1
             }
             else
             {
-                richTextBox1.AppendText("6");
+                richTextBox1.AppendText(text: "6");
             }
         }
 
@@ -116,7 +116,7 @@ namespace BIVT_21_15_Liamin_Borodin.D.A_DZ_1
             }
             else
             {
-                richTextBox1.AppendText("7");
+                richTextBox1.AppendText(text: "7");
             }
         }
         private void Button12_Click(object sender, EventArgs e)
@@ -127,7 +127,7 @@ namespace BIVT_21_15_Liamin_Borodin.D.A_DZ_1
             }
             else
             {
-                richTextBox1.AppendText("8");
+                richTextBox1.AppendText(text: "8");
             }
         }
 
@@ -139,96 +139,17 @@ namespace BIVT_21_15_Liamin_Borodin.D.A_DZ_1
             }
             else
             {
-                richTextBox1.AppendText("9");
+                richTextBox1.AppendText(text: "9");
             }
         }
 
         private void Button14_Click(object sender, EventArgs e)
         {
-            if (richTextBox1.Text == "")
-            {
-                MessageBox.Show("Ошибка");
-            }
-            else
-            {
-                Calculator calculator = new Calculator(richTextBox1.Text);
-                textBox1.Text = calculator.vs[0].ToString();
-                textBox2.Text = calculator.vs[1].ToString();
-                textBox3.Text = calculator.vs[2].ToString();
-                textBox4.Text = calculator.vs[3].ToString();
-                textBox5.Text = calculator.vs[4].ToString();
-                textBox6.Text = calculator.vs[5].ToString();
-                textBox7.Text = calculator.vs[6].ToString();
-                textBox8.Text = calculator.vs[7].ToString();
-                textBox9.Text = calculator.vs[8].ToString();
-                textBox10.Text = calculator.vs[9].ToString();
-                textBox11.Text = calculator.vs[10].ToString();
-                textBox12.Text = calculator.vs[11].ToString();
-                richTextBox2.Text = $"x = {calculator.x}\t y = {calculator.y}\t z = {calculator.z}";
-            }
+            Calculator calculator = new Calculator(str_1: richTextBox1.Text);
+            calculator.Result();
         }
 
         private void RichTextBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TextBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TextBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TextBox3_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TextBox4_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TextBox5_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TextBox6_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TextBox7_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TextBox8_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TextBox9_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TextBox10_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TextBox11_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TextBox12_TextChanged(object sender, EventArgs e)
         {
 
         }
@@ -241,47 +162,93 @@ namespace BIVT_21_15_Liamin_Borodin.D.A_DZ_1
 
     internal class Calculator
     {
-        public float[] vs = new float[12];
-        public float x;
-        public float y;
-        public float z;
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+        public float[] Vs { get; set; } = new float[12];
+
         public Calculator(string str_1)
         {
-            string[] strArr = str_1.Split(' ');
-            for (int i = 0; i < strArr.Length; i++)
+            try
             {
-                vs[i] = int.Parse(strArr[i]);
+                for (int i = 0; i < str_1.Split(' ').Length; i++)
+                {
+                    Vs[i] = int.Parse(((string[])str_1.Split(' '))[i]);
+                }
             }
-            float a1 = vs[0];
-            float b1 = vs[1];
-            float c1 = vs[2];
-            float d1 = vs[3];
-            float a2 = vs[4];
-            float b2 = vs[5];
-            float c2 = vs[6];
-            float d2 = vs[7];
-            float a3 = vs[8];
-            float b3 = vs[9];
-            float c3 = vs[10];
-            float d3 = vs[11];
-            x = (float)((d1 * b2 * c3) + (b1 * c2 * d3) + (c1 * d2 * b3) - (c1 * b2 * d3) - (b1 * d2 * c3) - (d1 * c2 * b3)) / (float)((a1 * b2 * c3) + (b1 * c2 * a3) + (c1 * a2 * b3) - (c1 * b2 * a3) - (b1 * a2 * c3) - (a1 * c2 * b3));
-            y = (float)((a1 * d2 * c3) + (c1 * a2 * d3) + (d1 * c2 * a3) - (c1 * d2 * a3) - (d1 * a2 * c3) - (a1 * c2 * d3)) / (float)((a1 * b2 * c3) + (b1 * c2 * a3) + (c1 * a2 * b3) - (c1 * b2 * a3) - (b1 * a2 * c3) - (a1 * c2 * b3));
-            z = (float)((a1 * b2 * d3) + (b1 * d2 * a3) + (d1 * a2 * b3) - (d1 * b2 * a3) - (b1 * a2 * d3) - (a1 * d2 * b3)) / (float)((a1 * b2 * c3) + (b1 * c2 * a3) + (c1 * a2 * b3) - (c1 * b2 * a3) - (b1 * a2 * c3) - (a1 * c2 * b3));
-            if (x == -0)
+            catch (Exception) { }
+            float a1 = Vs[0];
+            float b1 = Vs[1];
+            float c1 = Vs[2];
+            float d1 = Vs[3];
+            float a2 = Vs[4];
+            float b2 = Vs[5];
+            float c2 = Vs[6];
+            float d2 = Vs[7];
+            float a3 = Vs[8];
+            float b3 = Vs[9];
+            float c3 = Vs[10];
+            float d3 = Vs[11];
+            X = (float)((d1 * b2 * c3)
+                        + (b1 * c2 * d3)
+                        + (c1 * d2 * b3)
+                        - (c1 * b2 * d3)
+                        - (b1 * d2 * c3)
+                        - (d1 * c2 * b3)) /
+                (float)((a1 * b2 * c3)
+                        + (b1 * c2 * a3)
+                        + (c1 * a2 * b3)
+                        - (c1 * b2 * a3)
+                        - (b1 * a2 * c3)
+                        - (a1 * c2 * b3));
+            Y = (float)((a1 * d2 * c3)
+                        + (c1 * a2 * d3)
+                        + (d1 * c2 * a3)
+                        - (c1 * d2 * a3)
+                        - (d1 * a2 * c3)
+                        - (a1 * c2 * d3)) / 
+                (float)((a1 * b2 * c3)
+                        + (b1 * c2 * a3)
+                        + (c1 * a2 * b3)
+                        - (c1 * b2 * a3)
+                        - (b1 * a2 * c3)
+                        - (a1 * c2 * b3));
+            Z = (float)((a1 * b2 * d3)
+                        + (b1 * d2 * a3)
+                        + (d1 * a2 * b3)
+                        - (d1 * b2 * a3)
+                        - (b1 * a2 * d3)
+                        - (a1 * d2 * b3)) / 
+                (float)((a1 * b2 * c3)
+                        + (b1 * c2 * a3)
+                        + (c1 * a2 * b3)
+                        - (c1 * b2 * a3)
+                        - (b1 * a2 * c3)
+                        - (a1 * c2 * b3));
+            if (X == -0)
             {
-                x = 0;
+                X = 0;
             }
-            if (y == -0)
+            if (Y == -0)
             {
-                y = 0;
+                Y = 0;
             }
-            if (z == -0)
+            if (Z == -0)
             {
-                z = 0;
+                Z = 0;
             }
-            x.ToString();
-            y.ToString();
-            z.ToString();
+            X.ToString();
+            Y.ToString();
+            Z.ToString();
+        }
+        public void Result()
+        {
+            string result = $"Сиситема уравненеий:\n\n"
+                            + $"{Vs[0]}*x1 + {Vs[1]}*y1 + {Vs[2]} * z1 = {Vs[3]}\n"
+                            + $"{Vs[4]}*x2 + {Vs[5]}*y2 + {Vs[6]} * z2 = {Vs[7]}\n"
+                            + $"{Vs[8]}*x3 + {Vs[9]}*y3 + {Vs[10]} * z3 = {Vs[11]}\n\n"
+                            + $"Ответ: X = {X}, Y = {Y}, Z = {Z}";
+            MessageBox.Show(text: result);
         }
     }
 }
